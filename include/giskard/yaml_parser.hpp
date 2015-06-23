6696ea9756d0cb32740d27cbe9c07f3ec5a4191e
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-06-23 12:01:21 +0200

    Added parsing of frame-variables.

--- include/giskard/yaml_parser.hpp
+++ include/giskard/yaml_parser.hpp
@@ -6,13 +6,14 @@
 #include <kdl/expressiontree.hpp>
 #include <giskard/exceptions.hpp>
 #include <giskard/kdl_typedefs.hpp>
+#include <boost/assign/list_of.hpp>
 
 namespace giskard 
 {
   inline bool is_input_variable(const YAML::Node& node)
   {
     return node.IsMap() && node["name"] && node["type"] &&
-      (node["type"].as<std::string>().compare("INPUT-VAR") == 0);
+      (node["type"].as<std::string>().compare("INPUT-VARIABLE") == 0);
   }
 
   inline KDL::Expression<double>::Ptr parse_input_variable(const YAML::Node& node, 
@@ -24,6 +25,33 @@ namespace giskard
     KDL::InputTypePtr input(new KDL::InputType(var_index, 0.0));
     input->name = node["name"].as<std::string>();
     return input;
+  }
+
+  inline bool is_input_frame(const YAML::Node& node)
+  {
+    return node.IsMap() && node["name"] && node["type"] &&
+        (node["type"].as<std::string>().compare("INPUT-FRAME") == 0);
+  }
+
+  inline std::vector< KDL::Expression<double>::Ptr > parse_input_frame(
+      const YAML::Node& node, unsigned int start_var_index)
+  {
+    if(!is_input_frame(node))
+      throw YamlParserException("Given yaml-node does not represent an input-frame.");
+
+    std::vector<std::string> postfixes = boost::assign::list_of(" translation-x")
+        (" translation-y")(" translation-z")(" rotation-x")(" rotation-y")(" rotation-z");
+    std::string frame_name = node["name"].as<std::string>();
+
+    std::vector< KDL::Expression<double>::Ptr > result;
+    for(unsigned int i=0; i<postfixes.size(); ++i)
+    {
+      KDL::InputTypePtr input(new KDL::InputType(start_var_index + i, 0.0));
+      input->name = frame_name + postfixes[i];
+      result.push_back(input); 
+    }
+    
+    return result;
   }
 }
 
--- test/giskard/yaml_parser.cpp
+++ test/giskard/yaml_parser.cpp
@@ -11,7 +11,8 @@ class YamlParserTest : public ::testing::Test
 
 TEST_F(YamlParserTest, ParseInputVariable)
 {
-  YAML::Node valid_node = YAML::Load("{name: trans-x, type: INPUT-VAR}");
+  // Making sure we can parse a proper description
+  YAML::Node valid_node = YAML::Load("{name: trans-x, type: INPUT-VARIABLE}");
   EXPECT_TRUE(giskard::is_input_variable(valid_node));
   unsigned int input_index = 2;
 
@@ -22,7 +23,8 @@ TEST_F(YamlParserTest, ParseInputVariable)
   EXPECT_STREQ(input->name.c_str(), "trans-x");
   EXPECT_EQ(input->variable_number, input_index);
 
-  YAML::Node invalid_node1 = YAML::Load("{names: trans-x, type: INPUT-VAR}");
+  // Catching various improper descriptions
+  YAML::Node invalid_node1 = YAML::Load("{name: trans-x, type: INPUT-VAR}");
   YAML::Node invalid_node2 = YAML::Load("{name: trans-x, types: INPUT-VAR}");
   YAML::Node invalid_node3 = YAML::Load("{type: INPUT-VAR}");
   YAML::Node invalid_node4 = YAML::Load("{name: trans-x}");
@@ -43,5 +45,49 @@ TEST_F(YamlParserTest, ParseInputVariable)
   EXPECT_THROW(giskard::parse_input_variable(invalid_node4, input_index),
      giskard::YamlParserException); 
   EXPECT_THROW(giskard::parse_input_variable(invalid_node5, input_index),
+     giskard::YamlParserException); 
+};
+
+TEST_F(YamlParserTest, ParseInputFrame)
+{
+  // Making sure we can parse a proper description
+  YAML::Node valid_node = YAML::Load("{name: my frame, type: INPUT-FRAME}");
+  EXPECT_TRUE(giskard::is_input_frame(valid_node));
+
+  std::vector<std::string> input_names = boost::assign::list_of("my frame translation-x")
+      ("my frame translation-y")("my frame translation-z")("my frame rotation-x")
+      ("my frame rotation-y")("my frame rotation-z");
+
+  unsigned int start_index = 2;
+  std::vector< KDL::Expression<double>::Ptr > expressions = 
+      giskard::parse_input_frame(valid_node, start_index);
+
+  ASSERT_EQ(expressions.size(), input_names.size() );
+  for(unsigned int i=0; i<input_names.size(); ++i)
+  {
+    KDL::InputTypePtr input = boost::static_pointer_cast< KDL::InputType >(expressions[i]);
+   
+    EXPECT_STREQ(input->name.c_str(), input_names[i].c_str());
+    EXPECT_EQ(input->variable_number, start_index + i);
+  }
+
+  // Catching various improper descriptions
+  YAML::Node invalid_node1 = YAML::Load("{name: my frame, type: INPUT}");
+  YAML::Node invalid_node2 = YAML::Load("{name: my frame}");
+  YAML::Node invalid_node3 = YAML::Load("{type: INPUT-FRAME}");
+  YAML::Node invalid_node4 = YAML::Load("[1.0, 2.0, 3.0]");
+
+  EXPECT_FALSE(giskard::is_input_variable(invalid_node1));
+  EXPECT_FALSE(giskard::is_input_variable(invalid_node2));
+  EXPECT_FALSE(giskard::is_input_variable(invalid_node3));
+  EXPECT_FALSE(giskard::is_input_variable(invalid_node4));
+
+  EXPECT_THROW(giskard::parse_input_variable(invalid_node1, start_index),
+     giskard::YamlParserException); 
+  EXPECT_THROW(giskard::parse_input_variable(invalid_node2, start_index),
+     giskard::YamlParserException); 
+  EXPECT_THROW(giskard::parse_input_variable(invalid_node3, start_index),
+     giskard::YamlParserException); 
+  EXPECT_THROW(giskard::parse_input_variable(invalid_node4, start_index),
      giskard::YamlParserException); 
 };
