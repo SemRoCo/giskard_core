cd05544acf385c7d48544ef71ccdd3b692727fa3
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-06-23 16:13:19 +0200

    Slight re-ordering of functions in yaml-parser.

--- include/giskard/yaml_parser.hpp
+++ include/giskard/yaml_parser.hpp
@@ -6,19 +6,20 @@
 
 namespace giskard 
 {
+
+  bool is_input_list(const YAML::Node& node);
+
   bool is_input_variable(const YAML::Node& node);
 
-  KDL::Expression<double>::Ptr parse_input_variable(const YAML::Node& node, 
-      unsigned int var_index);
-
   bool is_input_frame(const YAML::Node& node);
+
+  std::vector< KDL::Expression<double>::Ptr > parse_input_list(const YAML::Node& node);
 
   std::vector< KDL::Expression<double>::Ptr > parse_input_frame(
       const YAML::Node& node, unsigned int start_var_index);
 
-  bool is_input_list(const YAML::Node& node);
-
-  std::vector< KDL::Expression<double>::Ptr > parse_input_list(const YAML::Node& node);
+  KDL::Expression<double>::Ptr parse_input_variable(const YAML::Node& node, 
+      unsigned int var_index);
 }
 
 #endif // GISKARD_YAML_PARSER_HPP
--- src/giskard/yaml-parser.cpp
+++ src/giskard/yaml-parser.cpp
@@ -5,21 +5,13 @@
 
 namespace giskard
 {
-  bool is_input_variable(const YAML::Node& node)
+  bool is_input_list(const YAML::Node& node)
   {
-    return node.IsMap() && node["name"] && node["type"] &&
-      (node["type"].as<std::string>().compare("INPUT-VARIABLE") == 0);
-  }
+    bool result = node.IsMap() && node["inputs"] && node["inputs"].IsSequence();
+    for(unsigned int i=0; i<node["inputs"].size(); ++i)
+      result &= (is_input_variable(node["inputs"][i]) || is_input_frame(node["inputs"][i]));
 
-  KDL::Expression<double>::Ptr parse_input_variable(const YAML::Node& node, 
-      unsigned int var_index)
-  {
-    if(!is_input_variable(node))
-      throw YamlParserException("Given yaml-node does not represent an input-variable.");
-
-    KDL::InputTypePtr input(new KDL::InputType(var_index, 0.0));
-    input->name = node["name"].as<std::string>();
-    return input;
+    return result;
   }
 
   bool is_input_frame(const YAML::Node& node)
@@ -28,34 +20,10 @@ namespace giskard
         (node["type"].as<std::string>().compare("INPUT-FRAME") == 0);
   }
 
-  std::vector< KDL::Expression<double>::Ptr > parse_input_frame(
-      const YAML::Node& node, unsigned int start_var_index)
+  bool is_input_variable(const YAML::Node& node)
   {
-    if(!is_input_frame(node))
-      throw YamlParserException("Given yaml-node does not represent an input-frame.");
-
-    std::vector<std::string> postfixes = boost::assign::list_of(" translation-x")
-        (" translation-y")(" translation-z")(" rotation-x")(" rotation-y")(" rotation-z");
-    std::string frame_name = node["name"].as<std::string>();
-
-    std::vector< KDL::Expression<double>::Ptr > result;
-    for(unsigned int i=0; i<postfixes.size(); ++i)
-    {
-      KDL::InputTypePtr input(new KDL::InputType(start_var_index + i, 0.0));
-      input->name = frame_name + postfixes[i];
-      result.push_back(input); 
-    }
-    
-    return result;
-  }
-
-  bool is_input_list(const YAML::Node& node)
-  {
-    bool result = node.IsMap() && node["inputs"] && node["inputs"].IsSequence();
-    for(unsigned int i=0; i<node["inputs"].size(); ++i)
-      result &= (is_input_variable(node["inputs"][i]) || is_input_frame(node["inputs"][i]));
-
-    return result;
+    return node.IsMap() && node["name"] && node["type"] &&
+      (node["type"].as<std::string>().compare("INPUT-VARIABLE") == 0);
   }
 
   std::vector< KDL::Expression<double>::Ptr > parse_input_list(const YAML::Node& node)
@@ -90,4 +58,35 @@ namespace giskard
     return results;
   }
 
+  std::vector< KDL::Expression<double>::Ptr > parse_input_frame(
+      const YAML::Node& node, unsigned int start_var_index)
+  {
+    if(!is_input_frame(node))
+      throw YamlParserException("Given yaml-node does not represent an input-frame.");
+
+    std::vector<std::string> postfixes = boost::assign::list_of(" translation-x")
+        (" translation-y")(" translation-z")(" rotation-x")(" rotation-y")(" rotation-z");
+    std::string frame_name = node["name"].as<std::string>();
+
+    std::vector< KDL::Expression<double>::Ptr > result;
+    for(unsigned int i=0; i<postfixes.size(); ++i)
+    {
+      KDL::InputTypePtr input(new KDL::InputType(start_var_index + i, 0.0));
+      input->name = frame_name + postfixes[i];
+      result.push_back(input); 
+    }
+    
+    return result;
+  }
+
+  KDL::Expression<double>::Ptr parse_input_variable(const YAML::Node& node, 
+      unsigned int var_index)
+  {
+    if(!is_input_variable(node))
+      throw YamlParserException("Given yaml-node does not represent an input-variable.");
+
+    KDL::InputTypePtr input(new KDL::InputType(var_index, 0.0));
+    input->name = node["name"].as<std::string>();
+    return input;
+  }
 }
