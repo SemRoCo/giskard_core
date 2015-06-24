0fa8b58fd47b0b7fae02a8b7605294a5d88d59a5
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-06-24 20:36:50 +0200

    Added yaml-parsing of nested expressions.

--- include/giskard/yaml_parser.hpp
+++ include/giskard/yaml_parser.hpp
@@ -20,6 +20,25 @@ namespace YAML {
         node["upper_velocity_limit"];
   }
 
+  inline bool is_named_expression_spec (const Node& node)
+  {
+    return node.IsMap() && (node.size() == 3) &&
+        node["name"] && node["type"] && node["inputs"] &&
+        node["inputs"].IsSequence();
+  }
+
+  inline bool is_anonymous_expression_spec (const Node& node)
+  {
+    return node.IsMap() && (node.size() == 2) &&
+        node["type"] && node["inputs"] && node["inputs"].IsSequence();
+  }
+
+  inline bool is_expression_spec(const Node& node)
+  {
+    return node.IsScalar() || is_named_expression_spec(node) ||
+       is_anonymous_expression_spec(node);
+  }
+
   template<>
   struct convert<giskard::ObservableSpec> {
     
@@ -34,6 +53,8 @@ namespace YAML {
       if(!is_observable_spec(node))
         return false;
   
+      rhs.clear();
+
       rhs.name_ = node["name"].as<std::string>();
       rhs.type_ = node["type"].as<std::string>();
       return true;
@@ -55,11 +76,66 @@ namespace YAML {
     static bool decode(const Node& node, giskard::ControllableSpec& rhs) {
       if(!is_controllable_spec(node))
         return false;
+
+      rhs.clear();
   
       rhs.name_ = node["name"].as<std::string>();
       rhs.lower_vel_limit_ = node["lower_velocity_limit"].as<double>();
       rhs.upper_vel_limit_ = node["upper_velocity_limit"].as<double>();
       rhs.weight_ = node["weight"].as<double>();
+
+      return true;
+    }
+  };
+
+  template<>
+  struct convert<giskard::ExpressionSpec> {
+    
+    static Node encode(const giskard::ExpressionSpec& rhs) {
+      Node node;
+
+      if(rhs.type_.compare("CONSTANT") == 0)
+      {
+        node = rhs.value_;
+      }
+      else if(rhs.type_.compare("REFERENCE") == 0)
+      {
+        node = rhs.name_;
+      } 
+      else 
+      {
+        if(rhs.name_.length() > 0)
+          node["name"] = rhs.name_;
+        node["type"] = rhs.type_;
+        for(unsigned int i=0; i<rhs.inputs_.size(); ++i)
+          node["inputs"][i] = rhs.inputs_[i];
+      }
+
+      return node;
+    }
+  
+    static bool decode(const Node& node, giskard::ExpressionSpec& rhs) {
+      if(!is_expression_spec(node))
+        return false;
+      rhs.clear();
+
+      if(node.IsScalar())
+      {
+        try 
+        {
+          rhs.value_ = node.as<double>();
+          rhs.type_ = "CONSTANT";
+        } catch(const YAML::RepresentationException& e) {
+          rhs.name_ = node.as<std::string>();
+          rhs.type_ = "REFERENCE";
+        }
+        return true;
+      }
+
+      rhs.type_ = node["type"].as<std::string>();
+      rhs.inputs_ = node["inputs"].as< std::vector<giskard::ExpressionSpec> >();
+      if(node["name"])
+        rhs.name_ = node["name"].as<std::string>();
 
       return true;
     }
--- test/giskard/yaml_parser.cpp
+++ test/giskard/yaml_parser.cpp
@@ -116,3 +116,103 @@ TEST_F(YamlParserTest, ParseControllables)
   EXPECT_DOUBLE_EQ(c_list2[0].upper_vel_limit_, c1_upper);
   EXPECT_DOUBLE_EQ(c_list2[1].upper_vel_limit_, c2_upper);
 };
+
+TEST_F(YamlParserTest, ParseExpressions)
+{
+  std::string ref_name = "cup frame rotation-z";
+  std::string rot_name = "cup rotation";
+  std::string cup_top_name = "cup top centroid";
+  std::string cup_bottom_name = "cup bottom centroid";
+  std::string cup_rel_name = "cup upright";
+  double constant1 = 2.3;
+  double constant2 = -1.1;
+  std::string constant1_input = boost::lexical_cast<std::string>(constant1);
+  std::string constant2_input = boost::lexical_cast<std::string>(constant2);
+  std::string anonymous_vector_input = "{type: VECTOR3, inputs: [" +
+      constant1_input + ", " + ref_name + ", " + constant2_input + "]}"; 
+  std::string named_rotation_input = "{type: ROTATION3, inputs: [" +
+      constant1_input + ", " + constant2_input + ", " + ref_name +
+      "], name: " + rot_name + "}";
+  std::string nested_input = "{name: " + cup_rel_name + ", type: SUBTRACTION, " +
+      "inputs: [{type: Z-COORDINATE-OF, inputs: [" + cup_top_name + "]}, " +
+      "{type: Z-COORDINATE-OF, inputs: [" + cup_bottom_name + "]}]}";
+
+  // PARSING A REFERENCE
+  YAML::Node node = YAML::Load(ref_name);
+  ASSERT_NO_THROW(node.as<giskard::ExpressionSpec>());
+  giskard::ExpressionSpec exp = node.as<giskard::ExpressionSpec>();
+  EXPECT_STREQ(exp.type_.c_str(), "REFERENCE");
+  EXPECT_STREQ(exp.name_.c_str(), ref_name.c_str());
+
+  // PARSING A CONSTANT
+  node = YAML::Load(constant1_input);
+  ASSERT_NO_THROW(node.as<giskard::ExpressionSpec>());
+  exp = node.as<giskard::ExpressionSpec>();
+  EXPECT_STREQ(exp.type_.c_str(), "CONSTANT");
+  EXPECT_DOUBLE_EQ(exp.value_, constant1);
+
+  // PARSING AN ANONYMOUS VECTOR3
+  node = YAML::Load(anonymous_vector_input);
+  ASSERT_NO_THROW(node.as<giskard::ExpressionSpec>());
+  exp = node.as<giskard::ExpressionSpec>();
+  EXPECT_STREQ(exp.type_.c_str(), "VECTOR3");
+  EXPECT_STREQ(exp.name_.c_str(), "");
+  EXPECT_EQ(exp.inputs_.size(), 3);
+  EXPECT_STREQ(exp.inputs_[0].type_.c_str(), "CONSTANT");
+  EXPECT_DOUBLE_EQ(exp.inputs_[0].value_, constant1);
+  EXPECT_STREQ(exp.inputs_[1].type_.c_str(), "REFERENCE");
+  EXPECT_STREQ(exp.inputs_[1].name_.c_str(), ref_name.c_str());
+  EXPECT_STREQ(exp.inputs_[2].type_.c_str(), "CONSTANT");
+  EXPECT_DOUBLE_EQ(exp.inputs_[2].value_, constant2);
+
+  // PARSING A NAMED ROTATION3
+  node = YAML::Load(named_rotation_input);
+  ASSERT_NO_THROW(node.as<giskard::ExpressionSpec>());
+  exp = node.as<giskard::ExpressionSpec>();
+  EXPECT_STREQ(exp.type_.c_str(), "ROTATION3");
+  EXPECT_STREQ(exp.name_.c_str(), rot_name.c_str());
+  EXPECT_EQ(exp.inputs_.size(), 3);
+  EXPECT_STREQ(exp.inputs_[0].type_.c_str(), "CONSTANT");
+  EXPECT_DOUBLE_EQ(exp.inputs_[0].value_, constant1);
+  EXPECT_STREQ(exp.inputs_[1].type_.c_str(), "CONSTANT");
+  EXPECT_DOUBLE_EQ(exp.inputs_[1].value_, constant2);
+  EXPECT_STREQ(exp.inputs_[2].type_.c_str(), "REFERENCE");
+  EXPECT_STREQ(exp.inputs_[2].name_.c_str(), ref_name.c_str());
+
+  // PARSING NESTED EXPRESSIONS
+  node = YAML::Load(nested_input);
+  ASSERT_NO_THROW(node.as<giskard::ExpressionSpec>());
+  exp = node.as<giskard::ExpressionSpec>();
+  EXPECT_STREQ(exp.type_.c_str(), "SUBTRACTION");
+  EXPECT_STREQ(exp.name_.c_str(), cup_rel_name.c_str());
+  ASSERT_EQ(exp.inputs_.size(), 2);
+  EXPECT_STREQ(exp.inputs_[0].name_.c_str(), "");
+  EXPECT_STREQ(exp.inputs_[0].type_.c_str(), "Z-COORDINATE-OF");
+  ASSERT_EQ(exp.inputs_[0].inputs_.size(), 1);
+  EXPECT_STREQ(exp.inputs_[0].inputs_[0].name_.c_str(), cup_top_name.c_str());
+  EXPECT_STREQ(exp.inputs_[0].inputs_[0].type_.c_str(), "REFERENCE"); 
+  EXPECT_STREQ(exp.inputs_[1].name_.c_str(), "");
+  EXPECT_STREQ(exp.inputs_[1].type_.c_str(), "Z-COORDINATE-OF");
+  ASSERT_EQ(exp.inputs_[1].inputs_.size(), 1);
+  EXPECT_STREQ(exp.inputs_[1].inputs_[0].name_.c_str(), cup_bottom_name.c_str());
+  EXPECT_STREQ(exp.inputs_[1].inputs_[0].type_.c_str(), "REFERENCE"); 
+
+  // ROUNDTRIP WITH YAML GENERATION
+  YAML::Node node2;
+  node2 = exp;
+  ASSERT_NO_THROW(node2.as<giskard::ExpressionSpec>());
+  giskard::ExpressionSpec exp2 = node2.as<giskard::ExpressionSpec>();
+  EXPECT_STREQ(exp2.type_.c_str(), "SUBTRACTION");
+  EXPECT_STREQ(exp2.name_.c_str(), cup_rel_name.c_str());
+  ASSERT_EQ(exp2.inputs_.size(), 2);
+  EXPECT_STREQ(exp2.inputs_[0].name_.c_str(), "");
+  EXPECT_STREQ(exp2.inputs_[0].type_.c_str(), "Z-COORDINATE-OF");
+  ASSERT_EQ(exp2.inputs_[0].inputs_.size(), 1);
+  EXPECT_STREQ(exp2.inputs_[0].inputs_[0].name_.c_str(), cup_top_name.c_str());
+  EXPECT_STREQ(exp2.inputs_[0].inputs_[0].type_.c_str(), "REFERENCE"); 
+  EXPECT_STREQ(exp2.inputs_[1].name_.c_str(), "");
+  EXPECT_STREQ(exp2.inputs_[1].type_.c_str(), "Z-COORDINATE-OF");
+  ASSERT_EQ(exp2.inputs_[1].inputs_.size(), 1);
+  EXPECT_STREQ(exp2.inputs_[1].inputs_[0].name_.c_str(), cup_bottom_name.c_str());
+  EXPECT_STREQ(exp2.inputs_[1].inputs_[0].type_.c_str(), "REFERENCE"); 
+};
