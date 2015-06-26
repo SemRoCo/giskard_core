b0c03a1182776fca10185a28ca1a5adede2610a8
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-06-26 16:01:08 +0200

    Removed unnecessary header files.

--- include/giskard/giskard.hpp
+++ include/giskard/giskard.hpp
@@ -1,9 +1,7 @@
 #ifndef GISKARD_GISKARD_HPP
 #define GISKARD_GISKARD_HPP
 
-#include <giskard/exceptions.hpp>
 #include <giskard/expressiontree.hpp>
-#include <giskard/kdl_typedefs.hpp>
 #include <giskard/structs.hpp>
 #include <giskard/yaml_parser.hpp>
 
