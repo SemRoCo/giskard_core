c87216c7e73b59be67db74b2ae0c015dc17fca81
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2015-06-28 15:51:36 +0200

    Bug-fix: Corrected wrong function-call in ExpressionArray::copy_results.

--- include/giskard/expression_arrays.hpp
+++ include/giskard/expression_arrays.hpp
@@ -143,7 +143,7 @@ namespace KDL
         {
           values_(i, 0) = expressions_[i]->value();
           for(size_t j=0; j<expressions_[i]->number_of_derivatives(); ++j)
-            derivatives_(i,j) = expressions_[i]->get_derivative(j);
+            derivatives_(i,j) = expressions_[i]->derivative(j);
         }
       }
   };
